0877f2bf03449c5b3021e872fa75180ea679277b
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-03-31 20:47:38 -0300

    mudança da cor e do texto do botão ao selecionar tres pratos

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
-
+let prato_selecionado =0;
+let bebida_selecionada=0;
+let sobremesa_selecionada=0;
 function pratoprincipal (x){
     const pratos = document.querySelectorAll(".principal .itens .item");
-    
+    prato_selecionado=1;
     if(x === 1){
         pratos[0].classList.add("checked");
         pratos[1].classList.remove("checked");
         pratos[2].classList.remove("checked");
         pratos[3].classList.remove("checked");
+        
     }
     if(x === 2){
         pratos[1].classList.add("checked");
@@ -26,10 +29,15 @@ function pratoprincipal (x){
         pratos[1].classList.remove("checked");
         pratos[2].classList.remove("checked");
     }
+    if(prato_selecionado===1 && bebida_selecionada === 1 && sobremesa_selecionada === 1){
+        const botao = document.querySelector("button");
+        botao.style.backgroundColor = "#32B72F";
+    }
 
 }
 function bebidas (x){
     const bebida = document.querySelectorAll(".bebida .itens .item");
+    bebida_selecionada=1;
     if(x === 1){
         bebida[0].classList.add("checked");
         bebida[1].classList.remove("checked");
@@ -54,9 +62,14 @@ function bebidas (x){
         bebida[1].classList.remove("checked");
         bebida[2].classList.remove("checked");
     }
+    if(prato_selecionado===1 && bebida_selecionada === 1 && sobremesa_selecionada === 1){
+        const botao = document.querySelector("button");
+        botao.style.backgroundColor = "#32B72F";
+    }
 }
 function sobremesas (x){
     const sobremesa = document.querySelectorAll(".sobremesa .itens .item");
+    sobremesa_selecionada=1;
     if(x === 1){
         sobremesa[0].classList.add("checked");
         sobremesa[1].classList.remove("checked");
@@ -81,4 +94,10 @@ function sobremesas (x){
         sobremesa[1].classList.remove("checked");
         sobremesa[2].classList.remove("checked");
     }
+    if(prato_selecionado===1 && bebida_selecionada === 1 && sobremesa_selecionada === 1){
+        const botao = document.querySelector("button");
+        botao.style.backgroundColor = "#32B72F";
+        botao.innerHTML = "<strong>Fechar pedido</strong>"
+    }
 }
+
